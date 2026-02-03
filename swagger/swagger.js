2758c59b502921a0326d1c0ac2e5c698c4f59802
Romel Carino <carinorome@gmail.com>
2026-02-03 09:10:59 +0800

    Make Swagger host environment-aware

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -1,12 +1,16 @@
 const swaggerAutogen = require('swagger-autogen')();
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const doc = {
   info: {
     title: 'Vehicle Management API',
     description: 'Documentation for the Vehicle Management API'
   },
-  host: 'localhost:3000',
-  schemes: ['http']
+  host: isProd
+    ? 'cse-341-final-project-1t9z.onrender.com'
+    : 'localhost:3000',
+  schemes: isProd ? ['https'] : ['http']
 };
 
 const outputFile = './swagger.json';
