ede026a797c887a721802636f6d687fbd57b1a04
Romel Carino <carinorome@gmail.com>
2026-02-13 15:30:09 +0800

    Revert "Merge pull request #5 from rcari013/week6-esteban-crud"
    This reverts commit 0e13cedc39a6bb96e93a45fb84ac178d0fe4115a, reversing
    changes made to a781a6806bc21708902487fdff1ef48e4e0708d7.

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,5 @@ router.use('/vehicles', require('./vehicles'));
 
 // Service routes
 router.use('/services', require('./services'));
-router.use('/users', require('./users'));
-router.use('/bookings', require('./bookings'));
-
 
 module.exports = router;
